84303ba1502a2e37c0d6c7d3c8c36b77a35b7b42
BEvanSchooten <47793774+BEvanSchooten@users.noreply.github.com>
2020-03-10 20:50:15 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(shinythemes)
 library(dplyr)
@@ -17,7 +8,8 @@ dagger <- function(X){
 }
 
 compareMatrix <- function(a,b){
-  isTRUE(all.equal(a,b))
+  max(isTRUE(all.equal(a,b)),isTRUE(all.equal(a,1i*b)),
+      isTRUE(all.equal(a,-1*b)),isTRUE(all.equal(a,-1i*b)))
 }
 
 # Pauli group
@@ -52,29 +44,71 @@ ClifM22 <- matrix((1/(sqrt(2)))*c(1,-1i,-1i,1),nrow = 2,byrow = TRUE)
 ClifM23 <- matrix((-1i/(sqrt(2)))*c(1,-1i,1i,-1),nrow = 2,byrow = TRUE)
 ClifM24 <- matrix((exp(-1i*pi/4)/(sqrt(2)))*c(1,-1i,1,1i),nrow = 2,byrow = TRUE)
 
-# Define UI for application
+GetGroupstructure <- function(MatrixList){
+  m <- length(MatrixList)
+  GroupStructure <- matrix(rep(0,m^2),nrow = m)
+  for(i in 1:m){ # Current state
+    for(k in 1:m){ # Gate
+      ki <- MatrixList[[k]] %*% MatrixList[[i]]
+      for(j in 1:m){ # Gate
+        if(compareMatrix(ki, MatrixList[[j]])){
+          GroupStructure[i,j] <- k
+          break
+        }
+      }
+    }
+  }
+  return(GroupStructure)
+}
+
+GetErrorStructure <- function(MatrixList){
+  m <- length(MatrixList)
+  ErrorGroupStructure <- array(rep(0,m^3),rep(m,3))
+  for(i in 1:m){ # Current error state
+    for(k in 1:m){ # Gate used
+      ki <- MatrixList[[k]] %*% MatrixList[[i]]
+      for(l in 1:m){ # Error gate used
+        lki <- MatrixList[[l]] %*%  ki
+        for(j in 1:m){ # Goal error state
+          if(compareMatrix(lki, MatrixList[[j]])){
+            ErrorGroupStructure[k,i,j] <- l
+            break
+          }
+        }
+      }
+    }
+  }
+  return(ErrorGroupStructure)
+}
+
+try(load("myRdata.Rdata"))
+
 ui <- fluidPage(theme = shinytheme("lumen"),
    
-   # Application title
-   titlePanel("Transition Matrix for Qbit error accumulation"),
+   titlePanel("Transition Matrices of Markov chains for error accumulation in quantum circuits"),
    
-   # Sidebar Input
    sidebarLayout(
       sidebarPanel(
-        selectInput(inputId = "Group", label=strong("Group"),
+        selectInput(inputId = "Group", label=strong("Pauli or Clifford Group"),
                     choices = c("Pauli","Clifford"),
                     selected = "Pauli"
         ),
-        
-        checkboxInput(inputId = "ManualPsi", label = strong("Manual Psi input")
+        selectInput(inputId = "PorQChoice", label=strong("P or Q matrix"),
+                    choices = c("P","Q"),
+                    selected = "P"
         ),
-        conditionalPanel(condition = "input.ManualPsi == true",
-                    textInput("Psi", 'Enter a Psi as vector (comma delimited)', "1.2,2+1i")
+        conditionalPanel(condition = "input.PorQChoice=='Q'",
+          selectInput(inputId = "GateChoice", label=strong("Which gate is used"),
+                      choices = c("I","X","Y","Z"),
+                      selected = "I"
+          )
         ),
-        checkboxInput(inputId = "UniformGate", label = strong("Uniform gate probabilites"),TRUE
-        ),
-        conditionalPanel(condition = "input.UniformGate == false",
+        conditionalPanel(condition = "input.PorQChoice=='P'",
+          checkboxInput(inputId = "UniformGate", label = strong("Uniform gate probabilites"),TRUE
+          ),
+          conditionalPanel(condition = "input.UniformGate == false",
                          textInput("GateProb", 'Enter a vector of gate probabilities (comma delimited)', "")
+          )
         ),
         checkboxInput(inputId = "UniformError", label = strong("Equal probability for all errors"),TRUE
         ),
@@ -86,55 +120,86 @@ ui <- fluidPage(theme = shinytheme("lumen"),
         ),
         actionButton("letsgo", "Run code"
         ),
-        downloadButton("downloadData", "Download as csv")
-        
+        downloadButton("downloadData", "Download as csv"
+        ),
+        tags$hr(),
+        downloadLink("readme","README"),
+        tags$hr()
       ),
       
-      # Mainpanel with outcome
       mainPanel(
-        h4("Look at this matrix"),
-        tableOutput("mytable")
+        conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+                         h4("The programm is running, please wait ..."),
+                         h4("This might take a couple of minutes.")),
+        h4(textOutput("caption")),
+        conditionalPanel(condition = "input.Group=='Clifford'",
+                         h4(textOutput("subcaption"))),
+        tableOutput("mytable"),
+        h4("Press the \"DOWNLOAD AS CSV\" button to download the full matrix in csv format"),
+        h4("The README contains more information about the program and how to use it")
       )
    )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   
-
-  MyTable <- reactive({
+  GateChoices <- reactive({
+    if(input$Group=="Pauli"){
+      c("I","X","Y","Z")
+    }else{
+      as.character(1:24)
+    }
+  })
+  
+  DefaultChoice <- reactive({
+    if(input$Group=="Pauli"){
+      "I"
+    }else{
+      "1"
+    }
+  })
+  
+  observe({
+  updateSelectInput(session, "GateChoice", choices = GateChoices(),selected = DefaultChoice())
+  })
+  SetupList <- reactive({
+    if(!exists("myList")){
+    MatrixList <- list(I,X,Y,Z)
+    PauliGS <- GetGroupstructure(MatrixList)
+    PauliES <- GetErrorStructure(MatrixList)
     
-    input$letsgo # Trigger a update
+    MatrixList <- list(ClifM01,ClifM02,ClifM03,ClifM04,ClifM05,ClifM06,ClifM07,ClifM08,ClifM09,ClifM10,ClifM11,ClifM12,
+                       ClifM13,ClifM14,ClifM15,ClifM16,ClifM17,ClifM18,ClifM19,ClifM20,ClifM21,ClifM22,ClifM23,ClifM24)
+    CliffordGS <- GetGroupstructure(MatrixList)
+    CliffordES <- GetErrorStructure(MatrixList)
+    myList <- list(PauliGS,PauliES,CliffordGS,CliffordES)
+    save(myList, file="myRdata.Rdata")
+    }
+    myList
+  })
+  
+  
+  MyPmatrix <- reactive({
     
+    input$letsgo
     # Get all variables
     Group <- isolate(input$Group)
-    ManualPsi <- isolate(input$ManualPsi)
-    Psi <- isolate(input$Psi)
     UniformGate <- isolate(input$UniformGate)
     GateProb <- isolate(input$GateProb)
     UniformError <- isolate(input$UniformError)
     ErrorProb <- isolate(input$ErrorProb)
     TotErrorProb <- isolate(input$TotErrorProb)
-    
-    
-    if(ManualPsi){
-      phi <- Psi
-    }else{
-      phi <-c(complex(real=runif(1),imaginary=runif(1)),complex(real=runif(1),imaginary=runif(1))) # Vector of two complex numbers
-    }
-    phi <- phi/sqrt(sum(Mod(phi)^2)) # Rescale
-    rho0 <- phi %*% dagger(phi) # Create rho0
-    
+
     if(Group == "Pauli"){
-      MatrixList <- list(I,X,Y,Z) # Set of gates
+      GroupStructure <- SetupList()[[1]]
+      ErrorGroupStructure <- SetupList()[[2]]
+      m <- 4
     }else{
-      MatrixList <- list(ClifM01,ClifM02,ClifM03,ClifM04,ClifM05,ClifM06,ClifM07,ClifM08,ClifM09,ClifM10,ClifM11,ClifM12,
-                         ClifM13,ClifM14,ClifM15,ClifM16,ClifM17,ClifM18,ClifM19,ClifM20,ClifM21,ClifM22,ClifM23,ClifM24)
+      GroupStructure <- SetupList()[[3]]
+      ErrorGroupStructure <- SetupList()[[4]]
+      m <- 24
     }
-    stateSpace <- lapply(MatrixList, function(x) x%*%rho0%*%dagger(x)) # Compute all states !! check for double states !! breath/depth search?
-    n <- length(stateSpace)
-    m <- length(MatrixList)
-    
+
     if(UniformGate){
       Pgate <- rep(1/m,m)
     }else{
@@ -149,61 +214,159 @@ server <- function(input, output) {
       Perror <- matrix(rep(errorlist,m),nrow = m, byrow = TRUE)
     }
     
-    # Step 3: determine transition matrix for non-errors
-    GroupStructure <- matrix(rep(0,n^2),nrow = n)
-    for(i in 1:n){ # Current state
-      for(j in 1:n){ # Goal state
-        for(k in 1:m){ # Gate
-          if(compareMatrix(MatrixList[[k]] %*% stateSpace[[i]] %*% dagger(MatrixList[[k]]),stateSpace[[j]])){
-            GroupStructure[i,j] <- k
-          }
-        }
-      }
-    }
-    
-    # Step 4: determine corresponding error for gate-couple
-    ErrorGroupStructure <- array(rep(0,n^3),rep(n,3))
-    for(i in 1:n){ # Current error state
-      for(j in 1:n){ # Goal error state
-        for(k in 1:m){ # Gate
-          for(l in 1:m){ # Error gate
-            if(compareMatrix(MatrixList[[l]] %*% MatrixList[[k]] %*% stateSpace[[i]] %*% dagger(MatrixList[[k]]) %*% dagger(MatrixList[[l]]), stateSpace[[j]])){
-              ErrorGroupStructure[k,i,j] <- l # gate,current,goal
-            }
-          }
-        }
-      }
-    }
-    
-    # Step 5: combine probabilities, compute P matrix
-    Pmatrix <- matrix(rep(0,n^4),nrow = n^2)
-    for(i in 1:n){ # Current state
-      for(j in 1:n){ # Current error state
-        for(k in 1:n){ # Goal state
-          for(l in 1:n){ # Goal error state
+    Pmatrix <- matrix(rep(0,m^4),nrow = m^2)
+    for(i in 1:m){ # Current state
+      for(j in 1:m){ # Current error state
+        for(k in 1:m){ # Goal state
+          for(l in 1:m){ # Goal error state
             Pmatrix[m*i-m+j,m*k-m+l] <- Pgate[GroupStructure[i,k]]*Perror[GroupStructure[i,k],ErrorGroupStructure[GroupStructure[i,k],j,l]]
           }
         }
       }
     }
     
-    # Step 6: enjoy
     Pmatrix
     
   })
   
+  MyQmatrix <- reactive({
+    
+    input$letsgo
+    # Get all variables
+    Group <- isolate(input$Group)
+    GateChoice <- isolate(input$GateChoice)
+    UniformError <- isolate(input$UniformError)
+    ErrorProb <- isolate(input$ErrorProb)
+    TotErrorProb <- isolate(input$TotErrorProb)
+    
+    if(Group == "Pauli"){
+      GroupStructure <- SetupList()[[1]]
+      ErrorGroupStructure <- SetupList()[[2]]
+      m <- 4
+    }else{
+      GroupStructure <- SetupList()[[3]]
+      ErrorGroupStructure <- SetupList()[[4]]
+      m <- 24
+    }
+    
+    if(UniformError){
+      r <- eval(parse(text=TotErrorProb))
+      Perror <- matrix(rep( c(1-r,rep(r/(m-1),m-1)),m),nrow = m, byrow = TRUE)
+    }else{
+      errorlist <- unlist(lapply(unlist(strsplit(ErrorProb,",")),function(x) eval(parse(text=x))))
+      Perror <- matrix(rep(errorlist,m),nrow = m, byrow = TRUE)
+    }
+    
+    Gate <- switch(GateChoice,
+                   "I" = 1,
+                   "X" = 2,
+                   "Y" = 3,
+                   "Z" = 4,
+                   "1" = 1,
+                   "2" = 2,
+                   "3" = 3,
+                   "4" = 4,
+                   "5" = 5,
+                   "6" = 6,
+                   "7" = 7,
+                   "8" = 8,
+                   "9" = 9,
+                   "10" = 10,
+                   "11" = 11,
+                   "12" = 12,
+                   "13" = 13,
+                   "14" = 14,
+                   "15" = 15,
+                   "16" = 16,
+                   "17" = 17,
+                   "18" = 18,
+                   "19" = 19,
+                   "20" = 20,
+                   "21" = 21,
+                   "22" = 22,
+                   "23" = 23,
+                   "24" = 24
+                   )
+    
+    Qmatrix <- matrix(rep(0,m^2),nrow=m)
+    for(i in 1:m){ # Current error state
+      for(j in 1:m){ # Goal error state
+        Qmatrix[i,j] <- Perror[Gate,ErrorGroupStructure[Gate,i,j]]
+      }
+    }
+    
+    Qmatrix
+    
+  })
+  
+  DisplayMatrix <- reactive({
+    input$letsgo
+    PorQchoice <- isolate(input$PorQChoice)
+    Group <- isolate(input$Group)
+    if(PorQchoice=="P"){
+      if(Group=="Pauli"){
+        MyPmatrix()
+      }else{
+        MyPmatrix()[1:12,1:12]
+      }
+    }else{
+      if(Group=="Pauli"){
+        MyQmatrix() 
+      }else{
+        MyQmatrix()[1:6,1:6]
+      }
+    }
+  })
+  
+  FullMatrix <- reactive({
+    input$letsgo
+    PorQchoice <- isolate(input$PorQChoice)
+    if(PorQchoice=="P"){
+      MyPmatrix()
+    }else{
+      MyQmatrix()
+    }
+  })
+  
+  output$caption <- renderText({
+    input$letsgo
+    PorQchoice <- isolate(input$PorQChoice)
+    Group <- isolate(input$Group)
+    GateChoice <- isolate(input$GateChoice)
+    if(PorQchoice=="P"){
+      if(Group=="Pauli"){
+        paste("The transition matrix P for the process Z_t = (X_t,Y_t) for the Pauli group and the given probabilities")
+      }else{
+        paste("The first 12x12 elements of the transition matrix P for the process Z_t = (X_t,Y_t) for the Clifford group and the given probabilities. WARNING: Not the full matrix")
+      }
+    }else{
+      if(Group=="Pauli"){
+        paste("The transition matrix Q_t for the process Y_t when gate",GateChoice,"is applied")
+      }else{
+        paste("The first 6x6 elements of the transition matrix Q_t for the process Y_t when gate",GateChoice,"is applied. WARNING: Not the full matrix")
+      }
+    }
+  })
+  
   output$mytable <- renderTable({
-    MyTable()[1:16,1:16]
+    DisplayMatrix()
   }, digits = 5,colnames = FALSE)
   
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste("Pmatrix.csv")
+      paste("Matrix.csv")
     },
     content = function(file) {
-      MyDf <- as.data.frame(MyTable())
+      MyDf <- as.data.frame(FullMatrix())
       names(MyDf) <- c()
       write.csv(MyDf, file, row.names = FALSE)
+    }
+  )
+  
+  output$readme <- downloadHandler(
+    filename = "README.pdf",
+    content = function(file) {
+      file.copy("www/readmefile.pdf", file)
     }
   )
 
